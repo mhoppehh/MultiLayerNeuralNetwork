33a13c68548bb7895bf9f7536404f654f0dd8844
mhoppehh <mhoppehh@gmail.com>
2018-02-20 15:02:18 -0600

    Completed methods for loading the data and randomizing the weights and biases of the SigNeurons.

--- src/DynamicNeuralNetwork.java
+++ src/DynamicNeuralNetwork.java
@@ -1,22 +1,20 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class DynamicNeuralNetwork
 {
 	String file_name = "data_compact.csv";
 	
-	int NUMBER_INPUT_NEURONS = 784;
-    int NUMBER_HIDDEN_NEURONS[] = 
-    		new int[] {30, 30, 30};
+	int NUMBER_INPUT_NEURONS = 10;
+    int NUMBER_HIDDEN_NEURONS[] = //At least 1
+    		new int[] {10, 10, 10};
     int NUMBER_OUTPUT_NEURONS = 10;
-    int TOTAL_NUMBER_OF_LAYERS = 
-    		NUMBER_INPUT_NEURONS +
-    		NUMBER_HIDDEN_NEURONS.length +
-    		NUMBER_OUTPUT_NEURONS;
-    int TOTAL_DATA_SIZE = 50000;
-    int TRAINING_PICS = 30000;
-    int TEST = 10000;
+    int TOTAL_NUMBER_OF_LAYERS = NUMBER_HIDDEN_NEURONS.length +	2;
+    int TOTAL_DATA_SIZE 	= 50000;
+    int TRAINING_PICS 		= 50000;
+    int TEST 				= 10000;
     
     NeuralNetwork nn;
     
@@ -25,16 +23,50 @@ public class DynamicNeuralNetwork
     }
     
     public DynamicNeuralNetwork() {
+    	
+    	GUI g = new GUI();
+    	
     	startTimer();
     	
     	init_neural_network();
     	
-    	Data train_data = load_training_data(file_name);
-    	Data test_data 	= load_testing_data	(file_name);
+    	Data train_data = load_training_data(file_name, 10, 784);
+    	Data test_data 	= load_testing_data	(file_name, 10, 784);
+    	
+    	randomize_weights_biases();
+    	
+    	int EPOCHS = 20;
+        int M_BATCH_SIZE = 10;
+        int LEARNING_RATE = 3;
     	
     }
     
-    private Data load_training_data(String file_name) {
+    private void randomize_weights_biases() {
+    	System.out.println("Assigning random values...");
+    	for(int i = 0; i < NUMBER_HIDDEN_NEURONS.length; i++){
+    		for (int j = 0; j < NUMBER_HIDDEN_NEURONS[i]; j++){
+    			nn.hidden[i][j] = new SigmoidNeuron(0.0,new double[nn.sizes[i]], 0.0, new double[nn.sizes[i]]);
+	            nn.hidden[i][j].bias = Math.random()*2.0 - 1.0;
+	            nn.hidden[i][j].dBias = 0;
+	            for (int k = 0; k < NUMBER_INPUT_NEURONS; k++){
+	                nn.hidden[i][j].weights[k] = Math.random()*2.0 - 1.0;
+	                nn.hidden[i][j].dWeights[j] = 0;
+	            }
+    			
+    		}
+    	}
+    	for (int i = 0; i < NUMBER_OUTPUT_NEURONS; i++){
+            nn.output[i] = new SigmoidNeuron(0.0,new double[nn.sizes[Math.max(0,nn.sizes.length - 2)]], 0.0, new double[nn.sizes[Math.max(0,nn.sizes.length - 2)]]);
+            nn.output[i].bias = Math.random()*2.0 - 1.0;
+            nn.output[i].dBias = 0;
+            for (int j = 0; j < NUMBER_HIDDEN_NEURONS[NUMBER_HIDDEN_NEURONS.length - 1]; j++){
+                nn.output[i].weights[j] = Math.random()*2.0 - 1.0;
+                nn.output[i].dWeights[j] = 0;
+            }
+        }
+    }
+    
+    private Data load_training_data(String file_name, int output_length, int input_length) {
     	Data d;
     	Picture pics[] = new Picture[TOTAL_DATA_SIZE];
         System.out.println("Loading training data...");
@@ -45,9 +77,10 @@ public class DynamicNeuralNetwork
         try {
 			reader = new BufferedReader(new FileReader(f));
 	        for (int p = 0; p < TOTAL_DATA_SIZE; p++){
-	            String answer = reader.readLine().replaceAll(" ", "");
-	            pics[p] = new Picture(new double[784], new int[10]);
-	            for (int y = 0; y < 10; y++){
+	        	String answer = reader.readLine();
+	            answer = answer.replaceAll(" ", "");
+	            pics[p] = new Picture(new double[input_length], new int[output_length]);
+	            for (int y = 0; y < output_length; y++){
 	                int index = answer.indexOf(",");
 	                String a = answer.substring(0,index);
 	                pics[p].output[y] = Integer.parseInt(a);
@@ -55,14 +88,14 @@ public class DynamicNeuralNetwork
 	        }
 	
             String string = reader.readLine().replaceAll(" ", "");
-            for (int x = 0; x < 784; x++){
+            for (int x = 0; x < input_length; x++){
                 int index = string.indexOf(",");
                 String pixel = string.substring(0,index);
                 pics[p].pixels[x] = (double)(Integer.parseInt(pixel)) / 256;
                 string = string.substring(index + 1, string.length());
             }
 	
-	        }      
+	        }
 	        d = new Data(pics, pics.length);
 	        reader.close();
 	    	return d;
@@ -72,7 +105,7 @@ public class DynamicNeuralNetwork
 		}
     }
     
-    private Data load_testing_data(String file_name) {
+    private Data load_testing_data(String file_name, int output_length, int input_length) {
     	Data t;
     	Picture[] pics = new Picture[TEST];
         System.out.println("Loading testing data...");
@@ -80,10 +113,10 @@ public class DynamicNeuralNetwork
 		try {
 			reader = new BufferedReader(new FileReader(new File(file_name)));
 	        for (int p = 0; p < TEST; p++){
-	            pics[p] = new Picture(new double[784], new int[10]);
+	            pics[p] = new Picture(new double[input_length], new int[output_length]);
 	
 	            String answer = reader.readLine().replaceAll(" ", "");
-	            for (int y = 0; y < 10; y++){
+	            for (int y = 0; y < output_length; y++){
 	                int index = answer.indexOf(",");
 	                String a = answer.substring(0,index);
 	                pics[p].output[y] = Integer.parseInt(a);
@@ -91,7 +124,7 @@ public class DynamicNeuralNetwork
 	            }
 	
 	            String string = reader.readLine().replaceAll(" ", "");
-	            for (int x = 0; x < 784; x++){
+	            for (int x = 0; x < input_length; x++){
 	                int index = string.indexOf(",");
 	                String pixel = string.substring(0,index);
 	                pics[p].pixels[x] = (double)(Integer.parseInt(pixel)) / 256;
---
+++ src/GUI.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.awt.image.BufferedImage;
+import java.io.FileReader;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+public class GUI
+{
+    JFrame frame;
+    public GUI(){
+        frame = new JFrame();
+    }
+
+    public static BufferedImage createResizedCopy(Image originalImage, 
+    int scaledWidth, int scaledHeight, 
+    boolean preserveAlpha)
+    {
+        int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, imageType);
+        Graphics2D g = scaledBI.createGraphics();
+        if (preserveAlpha) {
+            g.setComposite(AlphaComposite.Src);
+        }
+        g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null); 
+        g.dispose();
+        return scaledBI;
+    }
+
+    public static BufferedImage getImage(Picture[] pics){
+        int n = (int)(Math.ceil(Math.sqrt(pics.length)));
+        BufferedImage image = new BufferedImage(n * 28 * 4,n * 28 * 4, BufferedImage.TYPE_INT_ARGB);
+        Graphics g = image.getGraphics();
+        for(int i = 0;i < pics.length; i++){
+            BufferedImage icon = getLetterIcon(pics[i].pixels);
+            g.drawImage(icon, (i % n) * 28 * 4, (i / n) * 28 * 4, null);
+        }
+        return image;
+    }
+    
+    public static BufferedImage getLetterIcon(double pixels[]){
+        BufferedImage image = new BufferedImage(28,28, BufferedImage.TYPE_INT_ARGB);
+        Graphics g = image.getGraphics();
+
+        for(int i = 0; i < pixels.length; i++){
+            int value = (int)(255 - 255 * pixels[i]);
+            image.setRGB(i % 28, i / 28, new Color(value, value, value).getRGB());
+        }
+        System.out.println();
+
+        return createResizedCopy(image,(int)(28 * 4), (int)(28 * 4),false);
+        //return image;
+    }
+
+    public void displayData(Data data, ArrayList<double[]> output){
+        BufferedImage image = getImage(data.data);
+        JLabel lblimage = new JLabel(new ImageIcon(image));
+        lblimage.addMouseMotionListener(new MouseAdapter() {
+                @Override
+                public void mouseMoved(MouseEvent e) {
+                    int x = e.getX();
+                    int y = e.getY();
+                    int index = ((x / 28)) + (y / 28) * 10;
+                    String s = "";
+                    if(output != null && index < output.size()){
+                        for(int i = 0; i < output.get(index).length;i++){
+                            s += "\n" + i + " : ";
+                            for(int j = 0; j++ < output.get(index)[i] * 100;s += "*"){}
+                        }
+                    }
+                    System.out.println('\u000C');
+                    System.out.println(s);
+                }
+            });
+        frame.getContentPane().add(lblimage, BorderLayout.CENTER);
+        frame.setVisible(true);
+        frame.pack();
+        frame.repaint();
+    }
+    
+    public void displayPicture(double[] pixels, double scale){
+        BufferedImage image = getImage(new Picture[]{new Picture(pixels, null)});
+        image = createResizedCopy(image,(int)(28 * scale), (int)(28 * scale),false);
+        JLabel lblimage = new JLabel(new ImageIcon(image));
+        frame.getContentPane().add(lblimage, BorderLayout.CENTER);
+        frame.setVisible(true);
+        frame.pack();
+        frame.repaint();
+    }
+}
